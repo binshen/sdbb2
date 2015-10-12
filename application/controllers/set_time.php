0ff4af0b0cfb445a941c06c6bf91d8f5d5069b9b
yaobin1340 <645894453@qq.com>
2015-10-12 15:12:43 +0800

    32132113

--- application/controllers/set_time.php
+++ application/controllers/set_time.php
@@ -90,31 +90,9 @@ class Set_time extends CI_Controller {
     	$this->email->message($content);
     	//$this->email->attach('application\controllers\1.jpeg');			//相对于index.php的路径
     	$this->email->send();
-    	echo $this->email->print_debugger();
-    
+
     	//return $this->email->print_debugger();		//返回包含邮件内容的字符串，包括EMAIL头和EMAIL正文。用于调试。
     }
-    
-//     public function test(){
-//     	$rs = $this->sysconfig_model->send_message();
-//     	foreach($rs as $k=>$v){
-//     		$user = $this->message['user'];
-// 			$pwd = md5($this->message['pwd']);
-//     		$phone = $v['phone'];
-// 			$p_next = $v['next_phone'];
-			
-// 			$con = mb_convert_encoding('温馨提示：您的客户'.$v['name'].'已经到期！','gbk');
-// 			$url="http://api.52ao.com/?user={$user}&pass={$pwd}&mobile={$phone}&content={$con}"; 
-// 			$fcontent=@file_get_contents($url);
-			
-// 			if($p_next){
-// 				$con_next = mb_convert_encoding('温馨提示：客户'.$v['name'].'已经由您负责，请及时查看','gbk');
-// 				$url2="http://api.52ao.com/?user={$user}&pass={$pwd}&mobile={$p_next}&content={$con_next}"; 
-// 				$fcontent2=@file_get_contents($url2);
-// 			}
-			
-			
-//     	}
-//     }
+
     
 }
