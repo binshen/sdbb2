92ba5dedb5acb1c2b5442fa0a23427c3929214c7
yaobin1340 <645894453@qq.com>
2015-10-13 09:39:47 +0800

    1212212

--- application/controllers/wl_manager.php
+++ application/controllers/wl_manager.php
@@ -33,8 +33,8 @@ class Wl_manager extends MY_Controller {
     //默认首页
     public function index()
     {
-//     	$data = $this->user_model->get_bb_count();
-//     	$this->cismarty->assign('data',$data);
+    	$data = $this->user_model->get_bb_count();
+    	$this->cismarty->assign('data',$data);
     	$this->cismarty->display('user_index.html');
     }
     
