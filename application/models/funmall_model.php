ce9fa99329555a6f87f7e542e2085d5a3f8a1812
bin.shen <bluelazysb@hotmail.com>
2015-10-24 09:04:40 +0800

    123123

--- application/models/funmall_model.php
+++ application/models/funmall_model.php
@@ -13,10 +13,15 @@ class Funmall_model extends MY_Model {
 	}
 	
 	public function bindBroker($open_id, $broker_id) {
- 		$funmallDB = $this->load->database("funmall");
-		$funmallDB->from('wx_user');
-		$funmallDB->where('open_id', $open_id);
-		$wxUser = $funmallDB->get()->row_array();
+		try {
+			$funmallDB = $this->load->database("funmall");
+			$funmallDB->from('wx_user');
+			$funmallDB->where('open_id', $open_id);
+			$wxUser = $funmallDB->get()->row_array();
+		} catch (Exception $e) {
+			return $e->getMessage();
+		}
+ 		
 // 		if(empty($wxUser)) {
 // 			$wxUser = array(
 // 				'open_id' => $open_id,
