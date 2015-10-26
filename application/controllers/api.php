c95d659f198414bf4ff557e03ae7c7172172004e
bin.shen <bluelazysb@hotmail.com>
2015-10-26 09:20:17 +0800

    111

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -26,6 +26,23 @@ class Api extends MY_Controller {
 // 		$this->funmall_model->bindBroker('11211', 23);
 // 	}
 	
+	public function authorize() {
+		if (strpos($_SERVER['HTTP_USER_AGENT'], 'MicroMessenger') !== false) {
+			$code = $_GET['code'];
+			if(empty($code)){
+				$url = 'http://'.$_SERVER['SERVER_NAME'].$_SERVER["REQUEST_URI"];
+				redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=".APP_ID."&redirect_uri=".urlencode($url)."&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect");
+			} else {
+				$url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.APP_ID.'&secret='.APP_SECRET.'&code='.$code.'&grant_type=authorization_code';
+				$result = file_get_contents($url);
+				$jsonInfo = json_decode($result, true);
+				$open_id = $jsonInfo['open_id'];
+				echo $open_id;
+				die;
+			}
+		}
+	}
+	
 	public function index() {
 		
 		$echoStr = $_GET["echostr"];
